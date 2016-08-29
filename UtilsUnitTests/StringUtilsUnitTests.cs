2980b7bbb49da34625ca6741ad1b19a698c31a3d
Marcus Newman <marcusgeorgenewman@gmail.com>
2016-08-29 13:14:28 -0700

    Initial create, 2/3 unit tests passing

---
+++ UtilsUnitTests/StringUtilsUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReflectionUnitTesting;
+
+namespace Utils.UnitTests
+{
+    [TestClass]
+    public class StringUtilsUnitTests : UtilsBaseUnitTest
+    {
+        [TestMethod]
+        public void StringUtilsClassShouldExist()
+        {
+            GetType();
+        }
+
+        new Type GetType()
+        {
+            var assembly = GetAssembly();
+            return assembly.TypeExists("Utils", "StringUtils");
+        }
+
+        [TestMethod]
+        public void PluralizeMethodShouldExist()
+        {
+            var type = GetType();
+            type.MethodExists("Pluralize");
+        }
+    }
+}
---
+++ UtilsUnitTests/UtilsBaseUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReflectionUnitTesting;
+using System.Reflection;
+
+namespace Utils.UnitTests
+{
+    [TestClass]
+    public class UtilsBaseUnitTest
+    {
+        [TestMethod]
+        public void UtilsAssemblyShouldExist()
+        {
+            GetAssembly();
+        }
+
+        public Assembly GetAssembly()
+        {
+            return ReflectionAssert.AssemblyExists("Utils.dll");
+        }       
+    }
+}
